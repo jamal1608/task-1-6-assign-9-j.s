6547c5ab9fe240428991dba01577eea3e5d23d24
jamal1608 <ahmedbhatti909@gmail.com>
2024-03-14 00:31:49 +0500

    assign 9 task 1-6

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+//Task #01
+let studentNames = [];
+studentNames.push("Ahmed");
+studentNames.push("rashid");
+studentNames.push("jameel");
+
+
+//Task #02
+let games;
+games = ["hockey", "tennis", "cricket"];
+
+//task #03
+let numbers;
+numbers = [10, 20, -5, 2.34];
+
+//task #04
+
+let boolean;
+boolean = [true, false];
+
+//task #05
+
+let mixdata;
+mixdata = ["ahmed", 13, true, car];
+
